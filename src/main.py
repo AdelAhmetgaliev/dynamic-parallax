bd8c0142c4fda71148cd3600c27bce7757dcbdb6
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2024-10-14 18:44:14 +0300

    Added parallax calculation function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from pprint import pprint
 
-from doublestardata import read_data_from_file
+from doublestardata import DoubleStarData, read_data_from_file
+
+
+def calculate_parallax(star_data: DoubleStarData,
+                       first_star_mass: float, second_star_mass: float):
+    return (star_data.major_semi_axes /
+            (star_data.period ** 2 * (first_star_mass + second_star_mass)) ** (1 / 3))
 
 
 def main() -> None:
-    star_number = 1
+    star_number = 0
     star_data = read_data_from_file(star_number)
 
     pprint(star_data)
+    pprint(calculate_parallax(star_data, 1, 1))
 
 
 if __name__ == "__main__":

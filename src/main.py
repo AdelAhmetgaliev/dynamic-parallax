b7e8ea0648531d02e13dfb3cf9132fbbf0840e2b
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2024-10-14 18:52:28 +0300

    Added a function to calculate absolute stellar magnitude

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
+from math import log10
 from pprint import pprint
 
 from doublestardata import DoubleStarData, read_data_from_file
 
 
 def calculate_parallax(star_data: DoubleStarData,
-                       first_star_mass: float, second_star_mass: float):
+                       first_star_mass: float, second_star_mass: float) -> float:
     return (star_data.major_semi_axes /
             (star_data.period ** 2 * (first_star_mass + second_star_mass)) ** (1 / 3))
+
+
+def calculate_absolute_stellar_magnitude(
+        star_data: DoubleStarData, parallax: float) -> tuple[float, float]:
+    first_absolute_stellar_magnitude = star_data.first_stellar_magnitude + \
+                                       5 + 5 * log10(parallax)
+    second_absolute_stellar_magnitude = star_data.second_stellar_magnitude + \
+                                        5 + 5 * log10(parallax)
+
+    return first_absolute_stellar_magnitude, second_absolute_stellar_magnitude
 
 
 def main() -> None:
     star_number = 0
     star_data = read_data_from_file(star_number)
 
-    pprint(star_data)
-    pprint(calculate_parallax(star_data, 1, 1))
+    parallax = calculate_parallax(star_data, 1, 1)
+    absolute_stellar_magnitude = calculate_absolute_stellar_magnitude(
+        star_data, parallax)
+
+    pprint(f"Parallax: {parallax}")
+    pprint(absolute_stellar_magnitude)
 
 
 if __name__ == "__main__":

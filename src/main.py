e7fe0004fbd7e4f0c58faaf8813547afce28340a
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2024-10-15 10:43:45 +0300

    Added output to the console

--- src/main.py
+++ src/main.py
@@ -23,15 +23,24 @@ def calculate_absolute_stellar_magnitude(
 
 def main() -> None:
     star_number = 1
-    iteration_count = 10
+    iteration_count = 5
 
     star_data = read_data_from_file(star_number)
+
     first_star_mass = 1
     second_star_mass = 1
 
+    parallax = calculate_parallax(star_data, first_star_mass, second_star_mass)
+
+    print('Значения перед обработкой:')
+    print(
+        f'\tПервая звезда: {
+        first_star_mass:.3f} M☉, '
+        f'Вторая звезда: {
+        second_star_mass:.3f} M☉')
+    print(f'\tПараллакс: {parallax:.3f}"')
+
     for i in range(iteration_count):
-        parallax = calculate_parallax(
-            star_data, first_star_mass, second_star_mass)
         absolute_stellar_magnitude = calculate_absolute_stellar_magnitude(
             star_data, parallax)
         bolometric_correction = get_bolometric_correction(star_data)
@@ -46,8 +55,16 @@ def main() -> None:
 
         first_star_mass = get_corrected_mass(first_stellar_magnitude)
         second_star_mass = get_corrected_mass(second_stellar_magnitude)
+        parallax = calculate_parallax(
+            star_data, first_star_mass, second_star_mass)
 
-        print(f'{i}: {parallax:.3f}"')
+        print(f'Итерация №{i + 1}:')
+        print(
+            f'\tПервая звезда: {
+            first_star_mass:.3f} M☉, '
+            f'Вторая звезда: {
+            second_star_mass:.3f} M☉')
+        print(f'\tПараллакс: {parallax:.3f}"')
 
 
 if __name__ == "__main__":
